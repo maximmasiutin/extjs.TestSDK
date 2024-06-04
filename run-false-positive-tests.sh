be8b3626862be4c63c9677d7ca1d5cc0c7a1172e
SebastianKarpetaDev <161722641+SebastianKarpetaDev@users.noreply.github.com>
2024-06-04 14:02:43 +0200

    Update run-false-positive-tests.sh

--- run-false-positive-tests.sh
+++ run-false-positive-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 testRunerFileName='./run-tests.js';
-runParams='-sdk-url http://127.0.0.1:1841/ -no-webserver';
-nodeJs='/opt/homebrew/bin/node';
+runParams='-sdk-url http://127.0.0.1:1841/';
+nodeJs='node';
 disableLeakChecks='';
 
 # shellcheck disable=SC2039
