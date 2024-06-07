481cc16589d6078e94e04d2aa402bb8825eed511
Maxim Masiutin <maxim.masiutin@devtactics.net>
2024-06-08 01:37:08 +0300

    WIP

--- firefox-1-wrapper-run-tests.bash
+++ firefox-1-wrapper-run-tests.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-export SENCHATESTOUTPUTFNAME=firefox-1-run-tests.txt
-export SENCHATESTARCHNAME=firefox-1-run-tests.7z
 export SENCHATESTSCRIPTNAME=run-tests.js
 export SENCHATESTBROWSERS=firefox
+
+i=1
+export SENCHATESTOUTPUTFNAME=firefox-$i-run-tests.txt
+export SENCHATESTARCHNAME=firefox-$i-run-tests.7z
+
 ./run-test-script.bash
