80eafe7bbaa6e6eb21f64f486e0aca659e83d5bf
Maxim Masiutin <maxim.masiutin@devtactics.net>
2024-06-08 00:20:14 +0300

    Typo

--- firefox-1-wrapper-ross.bash
+++ firefox-1-wrapper-ross.bash
@@ -3,6 +3,7 @@
 export SENCHATESTOUTPUTFNAME=firefox-1-ross.txt
 export SENCHATESTARCHNAME=firefox-1-ross.7z
 export SENCHATESTSCRIPTNAME=index-ross.js
+export SENCHATESTBROWSERS=firefox
 ./run-test-script.bash
 
 
