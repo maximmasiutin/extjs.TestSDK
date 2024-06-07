5ac8d1859c3c80721309dc62fc05430664b182fa
Maxim Masiutin <maxim.masiutin@devtactics.net>
2024-06-07 22:51:51 +0300

    WIP

---
+++ chromium-firefox-1-wrapper-ross.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export SENCHATESTOUTPUTFNAME=chromium-firefox-1-ross.txt
+export SENCHATESTARCHNAME=chromium-firefox-1-ross.7z
+export SENCHATESTSCRIPTNAME=index-ross.js
+export SENCHATESTBROWSERS=chromium,firefox
+./run-test-script.bash
+
+
+
