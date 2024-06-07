66fe3f92585c4ee8b1946d015e143afbea895fd1
Maxim Masiutin <maxim.masiutin@devtactics.net>
2024-06-08 01:06:26 +0300

    Additional scripts

---
+++ chromium-1-wrapper-ross.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export SENCHATESTOUTPUTFNAME=chromium-1-ross.txt
+export SENCHATESTARCHNAME=chromium-1-ross.7z
+export SENCHATESTSCRIPTNAME=index-ross.js
+export SENCHATESTBROWSERS=chromium
+./run-test-script.bash
+
+
+
