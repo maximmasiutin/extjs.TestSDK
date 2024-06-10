a2a1ba996c00e8f7d59f71119717f139db86a35b
Maxim Masiutin <maxim.masiutin@devtactics.net>
2024-06-10 11:57:53 +0300

    WIP

---
+++ nodejsscript.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo ""
+node $SENCHATESTSCRIPTNAME -show-pass true -sdk-url "http://127.0.0.1:1841/" -toolkits classic,modern -browsers $SENCHATESTBROWSERS
+echo ""
---
+++ print_version_information.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo ""
+case "$OSTYPE" in
+  darwin*)  echo "OSX" ;; 
+  linux*)   lsb_release -a 2>&1 ;;
+  msys*)    echo "sw_vers" 2>&1;;
+  cygwin*)  echo "ver" ;;
+esac
+echo ""
+java -version 2>&1
+echo ""
+echo "node.js version: $(node --version)"
+echo "npm version: $(npm --version)"
+echo "Playwright version: $(npx playwright -V)"
+echo "PhantomJS version: $(phantomjs -v)"
+echo ""
+SENCHACMD=$(which sencha)
+if [ -x "$SENCHACMD" ]; then
+  $SENCHACMD which || echo ""
+else
+  echo "Sencha is not installed"
+fi
+echo ""
+openssl version -a
+echo ""
