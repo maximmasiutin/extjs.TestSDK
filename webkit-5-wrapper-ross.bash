bbe2a12394d11ad834bcda1c7b505f32d98fa7db
Maxim Masiutin <maxim.masiutin@devtactics.net>
2024-06-10 20:48:22 +0300

    WIP

---
+++ webkit-5-wrapper-ross.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export SENCHATESTSCRIPTNAME=index-ross.js
+export SENCHATESTBROWSERS=webkit
+export SENCHAOUTPUTFILENAMECLASS=webkit
+
+for i in $(seq 1 5);
+do
+export SENCHATESTOUTPUTFNAME="$SENCHAOUTPUTFILENAMECLASS-$i-ross-$OSTYPE.txt"
+echo "Output file: $SENCHATESTOUTPUTFNAME"
+export SENCHATESTARCHNAME="$SENCHAOUTPUTFILENAMECLASS-$i-ross-$OSTYPE.7z"
+echo "Archive name: $SENCHATESTARCHNAME"
+./run-test-script.bash
+done
+
+
+
+
+
